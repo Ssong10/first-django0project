f3b62ab617e3ac4263046c5ca9af18e2c23c4a0b
Ssong10 <leesy1403@naver.com>
2019-08-12 14:43:28 +0900

    05 | variable_routing

--- first_django/urls.py
+++ first_django/urls.py
@@ -19,9 +19,14 @@ from django.urls import path
 # 1. url 설정
 # pages app의 views.py 파일 불러오기
 from pages import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # 1. url 설정
     # path(url, 해당하는 views의 함수)
     path('', views.index),
+    # variable routing
+    # url의 특정 값을 변수처럼 활용
+    path('hello/<str:name>/', views.hello),
+    path('lotto/', views.lotto),
 ]
