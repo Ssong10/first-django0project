afcfb74caa32d81127ba67c8da630e2a2efcf363
Ssong10 <leesy1403@naver.com>
2019-08-13 17:59:19 +0900

    services | pull, push

--- services/views.py
+++ services/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
     return render(request, 'services/index.html')
+
+
+def artii(request):
+    return render(request, 'services/artii.html')
+
+def artii_result(request):
+    text = request.GET.get('text')
+    font = request.GET.get('font')
+    if font == 'base':
+        response = requests.get(f'http://artii.herokuapp.com/make?text={text}')
+    else:
+        response = requests.get(f'http://artii.herokuapp.com/make?text={text}&font={font}')
+    result = response.text
+    context = {
+        "result" : result
+    }
+    return render(request, 'services/artii_result.html',context)
+
+def push(request):
+    return render(request, 'services/push.html')
+
+def pull(request):
+    num = request.GET.get('num')
+    context = {
+        'num' : num
+    }
+    return render(request,'services/pull.html',context)
